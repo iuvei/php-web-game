c7a74d9b1b53c5778b29f4203a70bff2c6ad6bea
ace <ace39500@gmail.com>
2021-06-14 19:24:35 +0800

    新增计划任务

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use App\Console\Commands\kuai_san;
 use App\Console\Commands\kuai_che;
+use App\Console\Commands\lhc;
 use App\Console\Commands\ssc;
 use App\Console\Commands\statistics;
 use App\Console\Commands\create_lottery_record;
@@ -32,6 +33,7 @@ class Kernel extends ConsoleKernel
         $schedule->command(kuai_san::class, [])->everyMinute();
         $schedule->command(kuai_che::class, [])->everyMinute();
         $schedule->command(ssc::class, [])->everyMinute();
+        $schedule->command(lhc::class, [])->everyMinute();
         // 期号生成
         $schedule->command(create_lottery_record::class, [])->dailyAt('23:00');
         // 统计昨天报表
@@ -41,10 +43,12 @@ class Kernel extends ConsoleKernel
         $schedule->command(kuai_san::class, [5])->everyThreeMinutes();
         $schedule->command(kuai_che::class, [7])->everyThreeMinutes();
         $schedule->command(ssc::class, [9])->everyThreeMinutes();
+        $schedule->command(lhc::class, [12])->everyThreeMinutes();
         // 每5分钟执行
         $schedule->command(kuai_san::class, [6])->everyFiveMinutes();
         $schedule->command(kuai_che::class, [8])->everyFiveMinutes();
         $schedule->command(ssc::class, [10])->everyFiveMinutes();
+        $schedule->command(lhc::class, [13])->everyFiveMinutes();
 
 
     }
