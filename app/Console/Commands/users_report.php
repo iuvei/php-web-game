6a74956e640cd86b5b718630117cf02d3665322c
ace <ace39500@gmail.com>
2021-06-15 21:15:34 +0800

    用户报表统计增加日期筛选

--- app/Console/Commands/users_report.php
+++ app/Console/Commands/users_report.php
@@ -44,7 +44,7 @@ class users_report extends Command
         $type = $this->argument('type');
         $day = $type == 1 ? Carbon::today()->toDateString() : Carbon::yesterday()->toDateString();
         $res = UsersAccountChange::select('user_id',DB::raw('SUM(IF( type = 3, money, 0 )) as bottom_pour'),DB::raw('SUM(IF( type = 4, money, 0 )) as bonus'),DB::raw('DATE(created_at) as d'))
-//            ->whereDate('created_at', $day)
+            ->whereDate('created_at', $day)
             ->groupBy('user_id','d')
             ->get();
         (new UsersReport())->insertOrUpdate($res);
